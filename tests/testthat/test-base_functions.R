301033934eea3a0b72394df9d78bf93850658abc
apguthrie <54636090+apguthrie@users.noreply.github.com>
2023-07-03 17:38:35 -0400

    added basic tests and updated roxygen package

--- tests/testthat/test-base_functions.R
+++ tests/testthat/test-base_functions.R
@@ -1,15 +1,35 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#
+test_that("LRT gives correct results",{
+
+  lrt_538 <- LLO_LRT(hockey$x, hockey$y)
+
+  # check that LLO_LRT gives correct p-value for fivethirtyeight
+  expect_equal(lrt_538$pval, 0.118396594)
+
+  # check test stat is consistent for both
+  expect_equal(lrt_538$test_stat, 4.2674306)
+
+  # Check params
+  expect_equal(lrt_538$est_params[1], 0.9453853)
+  expect_equal(lrt_538$est_params[2], 1.4014034)
+
 })
+#
+# test_that("LRT gives correct test stat",{
+#
+#   #lrt_538 <- LLO_LRT(hockey$x, hockey$y)
+#
+#   # check that LLO_LRT gives correct p-value for fivethirtyeight
+#   #expect_equal(lrt_538$pval, 0.118396594)
+#
+#   # check test stat is consistent for both
+#   #expect_equal(lrt_538$test_stat, 4.2674306)
+#
+#   # Check params
+#   #expect_equal(lrt_538$est_params[1], 0.9453853)
+#   #expect_equal(lrt_538$est_params[2], 1.4014034)
+#
+# })
 
-# check that in functions tests behave properly?
 
-# check that LLO_LRT gives correct p-value for fivethirtyeight
 
-# check that LLO_LRT gives correct p-value for random hockey
-
-# check that params are consistent for fivethirtyeight
-
-# check that params are consistent for random pundit
-
-# check test stat is consistent for both
