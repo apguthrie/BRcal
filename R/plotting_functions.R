e04c7d25e5506d65b94dc5a47250b0ca1b6e2b71
apguthrie <54636090+apguthrie@users.noreply.github.com>
2023-07-07 15:16:14 -0400

    started developing new lineplot function

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -34,13 +34,13 @@ plot_params <- function(z, len.out = 100,
 
   if(!contours_only){
     fields::image.plot(d, g, z, zlim = zlim, xlim = c(lower[1], upper[1]), ylim = c(lower[2], upper[2]),
-               main = paste0(ttle, ttle_extra),
-               xlab = xlab,
-               ylab = ylab,
-               sub = sub,
-               legend.mar = 9,
-               legend.lab = leg_lab,
-               legend.args = legend.args, ...)
+                       main = paste0(ttle, ttle_extra),
+                       xlab = xlab,
+                       ylab = ylab,
+                       sub = sub,
+                       legend.mar = 9,
+                       legend.lab = leg_lab,
+                       legend.args = legend.args, ...)
     if(!anyNA(cont_levels)){
       contour(d, g, z, add = TRUE, levels = cont_levels, col = contour_color,
               drawlabels = drawlabels, lwd=lwd, labcex=labcex)
@@ -115,3 +115,132 @@ lineplot <- function(df, ttle="Line Plot", ylab="Probability", xlab = "Posterior
   return(lines)
 
 }
+
+lineplot_dev <- function(x, y, t=NULL, delta=NULL, gamma=NULL, ttle="Line Plot", ylab="Probability",
+                         xlab = "Posterior Model Probability",
+                         font_size_lp=5,
+                         outside_only = FALSE, pt_size = 0.75, ln_size = 0.25,
+                         pt_alpha = 0.35, ln_alpha = 0.25, font_base = 10,
+                         ylim=c(0,1), breaks=seq(0,1,by=0.2)){
+
+
+  # check validity of x,y inputs
+
+  # numeric
+  if(any(class(x) != "numeric")) stop("x must be non-empty numeric vector")
+  if(any(class(y) != "numeric")) stop("y must be non-empty numeric vector")
+
+  # same length
+  if(length(x) != length(y)) stop("x and y must be the same length")
+
+  # y binary
+  if(any(!(y %in% c(0,1)))) stop('y must be numeric vector of 1s (event) and 0s (nonevent)')
+
+  # x between 0,1
+  if(any(!(x < 0 || x > 1))) stop('x must be numeric vector in [0,1]')
+
+
+  # check that EITHER t or delta and gamma are specified
+
+  if(is.null(delta) != is.null(gamma)) stop('delta and gamma must either both be specified or NULL')
+
+  if(is.null(delta) & is.null(t)) stop('EITHER t OR delta and gamma must be specified, both are NULL')
+
+  if(!is.null(t) & !is.null(delta)) stop('EITHER t OR delta and gamma must be specified, both are specified')
+
+
+  if(!is.null(delta)){
+    # check validity of delta, gamma
+    # numeric
+    if(any(class(delta) != "numeric")) stop("delta must be non-empty numeric vector")
+    if(any(class(gamma) != "numeric")) stop("gamma must be non-empty numeric vector")
+
+    # same length
+    if(length(delta) != length(gamma)) stop("delta and gamma must be the same length")
+
+    # correct range for delta
+    # Gamma is fine without checking because we've already checked it's numeric vector which can only hold real numbers
+    if(any(delta <= 0)) stop("delta must be greater than 0")
+
+
+  }
+
+  # check validity of t
+  if(!is.null(t)){
+    # numeric
+    if(any(class(t) != "numeric")) stop("t must be non-empty numeric vector")
+
+    # correct range for t
+    if(any(!(t < 0 || t > 1))) stop('x must be numeric vector in [0,1]')
+  }
+
+  # CHECK GRAPHING PARAMS
+
+
+
+  # GET DATA IN RIGHT FORM
+
+  # get posterior model probabilities
+
+  # create data frame to hold params &
+
+  # for original
+  bt <- bayes_testing(x, y)
+
+  # for all sets of passed delta, gamma
+  # recalibrate, then pass to bayes_testing
+  for(i in 1:length(delta)){
+
+  }
+
+  font_size_lp <- font_base
+
+  requireNamespace(ggplot2)
+
+  if(!outside_only){
+
+    lines <- ggplot(data = df, mapping = aes(x = post, y = probs)) +
+      geom_point(aes(color = outcome), alpha = pt_alpha, size = pt_size,
+                 show.legend = FALSE) +
+      geom_line(aes(group=pairing, color = outcome), size = ln_size, alpha = ln_alpha,
+                show.legend = FALSE) +
+      labs(x = xlab,
+           y = ylab) +
+      ggtitle(ttle) +
+      theme_bw(base_size = font_size_lp) +
+      scale_y_continuous(breaks = breaks,
+                         limits = ylim,
+                         expand = c(0, 0))+
+      scale_color_manual(values = c("blue", "red")) +
+      scale_x_discrete(expand = c(0, 0.075))
+    # theme(axis.title.x = element_blank(),
+    #       axis.title.y = element_blank())
+    # theme(axis.title.x = element_text(size = font_size_lp),
+    #       axis.title.y = element_text(size = font_size_lp),
+    #       axis.text = element_text(size = font_size_lp),
+    #       title = element_text(size = font_size_lp))
+  } else {
+
+    lines <- ggplot(data = df, mapping = aes(x = post, y = probs)) +
+      geom_point(aes(color = pundit), alpha = pt_alpha, size = pt_size,
+                 show.legend = FALSE) +
+      geom_line(aes(group=c(pairing,pundit), color = pundit), size = ln_size, alpha = ln_alpha,
+                show.legend = FALSE) +
+      labs(x = xlab,
+           y = ylab) +
+      ggtitle(ttle) +
+      theme_bw() +
+      scale_y_continuous(breaks = breaks,
+                         limits = ylim,
+                         expand = c(0, 0))+
+      scale_color_manual(values = c("black", "red", "blue")) +
+      scale_x_discrete(expand = c(0, 0.075)) +
+      theme(axis.title.x = element_text(size = font_size_lp),
+            axis.title.y = element_text(size = font_size_lp),
+            axis.text = element_text(size = font_size_lp),
+            title = element_text(size = font_size_lp))
+  }
+  return(lines)
+
+}
+
