ea4d40cd204e23a83f05880e3c13f8742f9d0f54
apguthrie <54636090+apguthrie@users.noreply.github.com>
2023-09-22 09:37:52 -0400

    changed start_at_MLEs to true, added derivatives, changed from cobyla to slsqp

--- R/boldness_recalib.R
+++ R/boldness_recalib.R
@@ -2,7 +2,7 @@
 # also return br probs (add flag to toggle this)
 
 brcal <- function(x,y,t_level, x0=c(0.5,0.5), print_level=3, maxeval=100,
-                  xtol_rel=1.0e-8, start_at_MLEs=FALSE){
+                  xtol_rel=1.0e-8, start_at_MLEs=TRUE){
 
   if(start_at_MLEs){
     bt <- bayes_testing(x,y)
@@ -15,6 +15,30 @@ brcal <- function(x,y,t_level, x0=c(0.5,0.5), print_level=3, maxeval=100,
     return(-stats::sd(probs_new) )
   }
 
+  eval_grad_f <- function(x, probs, outs, t_level){
+
+
+    n <- length(probs)
+    probs_new <- LLO(probs, x[1], x[2])
+    sdp <- stats::sd(probs_new)
+    meanp <- mean(probs_new)
+    xmxg <- (probs * (1-probs))^x[2]
+    denom <- (x[1] * probs^x[2] + (1-probs)^x[2])^2
+    firstd <- xmxg/denom
+    meanxmxg <- mean(firstd)
+    pmmp <- (probs_new - meanp)
+    dxmxgl <- x[1] * xmxg * log(probs / (1-probs))
+    firstg <- dxmxgl/denom
+    meang <- mean(firstg)
+    innerd <- pmmp * (firstd - meanxmxg)
+    innerg <- pmmp * (firstg - meang)
+
+    grad_obj <- c((-1/((n-1) * sdp)) * sum(innerd),
+                  (-1/((n-1) * sdp)) * sum(innerg))
+
+    return(grad_obj)
+  }
+
   eval_g <- function(x, probs, outs, t_level){
 
     probs_new <- LLO(probs, x[1], x[2])
@@ -22,15 +46,45 @@ brcal <- function(x,y,t_level, x0=c(0.5,0.5), print_level=3, maxeval=100,
     return(c1)
   }
 
+  eval_grad_g <- function(x, probs, outs, t_level){
+
+
+    n <- length(probs)
+    probs_new <- LLO(probs, x[1], x[2])
+    bt <- bayes_testing(probs_new, outs)
+    pmp <- bt$posterior_model_prob
+    pmp2 <- pmp^2
+    dhat <- bt$est_params[1]
+    ghat <- bt$est_params[2]
+
+    pmps <- (pmp - pmp2)
+
+    firstd <- (ghat - 1) * outs * (1/x[1])
+    secondd <- (dhat * probs^(ghat * x[2])*ghat*x[1]^(ghat-1)) / (dhat * x[1]^ghat * probs^(ghat * x[2]) + (1-probs)^(ghat * x[2]))
+    thirdd <- (probs^x[2]) / (x[1] * probs^(x[2]) + (1-probs)^x[2])
+
+    firstg <- (ghat - 1) * (outs * log(probs) + (1-outs) * log(1-probs))
+    secondg <- (ghat*(dhat * x[1]^ghat * probs^(x[2] * ghat) * log(probs) + (1-probs)^(x[2] * ghat)*log(1-probs))) / (dhat * x[1]^ghat * probs^(x[2]*ghat) + (1-probs)^(x[2]*ghat))
+    thirdg <- (x[1] * probs^x[2] * log(probs) + (1-probs)^x[2] * log(1-probs)) / (x[1] * probs^x[2] + (1-probs)^x[2])
+
+    grad_obj <- c(pmps * sum(firstd - secondd + thirdd),
+                  pmps * sum(firstg - secondg + thirdg))
+
+
+    return(grad_obj)
+  }
+
   res <- nloptr::nloptr(x0 = x0,
                         eval_f = eval_f,
+                        eval_grad_f = eval_grad_f,
                         lb = c(0.0001, -15),
                         ub = c(Inf, 1074),
                         eval_g_ineq = eval_g,
-                        opts = list("algorithm"="NLOPT_LN_COBYLA",
+                        eval_jac_g_ineq  = eval_grad_g,
+                        opts = list("algorithm"="NLOPT_LD_SLSQP",
                                     "xtol_rel"=xtol_rel,
                                     "print_level"=print_level,
-                                    "maxeval"=maxeval),
+                                    "maxeval"=maxeval, "check_derivatives"=TRUE),
                         probs = x,
                         outs = y,
                         t_level = t_level)
@@ -38,31 +92,3 @@ brcal <- function(x,y,t_level, x0=c(0.5,0.5), print_level=3, maxeval=100,
 }
 
 
-brcal_dev <- function(x,y,t_level, x0=c(0.5,0.5), print_level=3){
-
-  eval_f <- function(x, probs, outs, t_level){
-    # this assumes x is vector?
-    probs_new <- LLO(probs, x[1], x[2])
-    return(-stats::sd(probs_new) )
-  }
-
-  eval_g <- function(x, probs, outs, t_level){
-
-    probs_new <- LLO(probs, x[1], x[2])
-    c1 <- bayes_testing_dev(probs_new, outs)$posterior_model_prob * -1 + t_level
-    return(c1)
-  }
-
-  res <- nloptr::nloptr(x0 = x0,
-                        eval_f = eval_f,
-                        lb = c(0.0001, -Inf),
-                        ub = c(Inf, Inf),
-                        eval_g_ineq = eval_g,
-                        opts = list("algorithm"="NLOPT_LN_COBYLA",
-                                    "xtol_rel"=1.0e-8,
-                                    "print_level"=print_level),
-                        probs = x,
-                        outs = y,
-                        t_level = t_level)
-  return(res)
-}
