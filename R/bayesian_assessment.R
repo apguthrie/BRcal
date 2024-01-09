503be6cc919a72f06b4e3adf2990e4c94afb94a4
apguthrie <54636090+apguthrie@users.noreply.github.com>
2024-01-09 17:05:02 -0500

    restructured into external / internal

--- R/bayesian_assessment.R
+++ R/bayesian_assessment.R
@@ -1,39 +1,7 @@
+######################################################
+#  External Functions                                #
+######################################################
 
-# BIC for this likelihood
-BIC_llo <- function(x, y, k, params = NA, lower = c(0.001, -5), upper = c(10,30)){
-  n <- length(x)
-  if(k == 0){
-    #suppressWarnings(if(is.na(params)) stop("must specify null params when k = 0"))
-    suppressWarnings(if(anyNA(params)) stop("must specify null params when k = 0"))
-
-    result <- -2*llo_lik(params = params, x = x, y = y, log = TRUE)
-  } else if(anyNA(params)){
-    # optBayes <- stats::optim(c(0.5, 0.5), llo_lik, x=x, y=y, log = TRUE, neg = TRUE, method = "L-BFGS-B",
-    #                   lower = lower, upper = upper)
-    # optBayes <- stats::optim(c(0.5, 0.5), llo_optim_wrap, x=x, y=y, method = "Nelder-Mead",
-    #                        neg = TRUE, log = TRUE)
-    optBayes <- llo_optim(x,y,lower,upper)
-    max_lik <- -optBayes$value
-    MLEs <- optBayes$par
-    result <- list(BIC = k * log(n) - (2 * max_lik),
-                   est_params = MLEs)
-  } else {
-    result <- list(BIC = k * log(n) - (2 * llo_lik(params = params, x = x, y = y, log = TRUE)),
-                   params = params)
-  }
-  return(result)
-}
-
-
-# Bayes factor - only approx BIC version for now
-bayes_factor <- function(BIC1, BIC2, approx = TRUE){
-  return(exp(-(1/2) * (BIC1 - BIC2)))
-}
-
-# Posterior model probability
-post_mod_prob <- function(BF){
-  return(1/(1+BF))
-}
 
 # Bayesian Calibration assessment function
 bayes_testing <- function(x, y, k = 2, params_null = c(1,1), params = NA, optim_details = FALSE, lower = c(0.001, -5), upper = c(10,30)){
@@ -75,6 +43,47 @@ bayes_testing <- function(x, y, k = 2, params_null = c(1,1), params = NA, optim_
 
   return(results)
 }
+
+######################################################
+#  Internal Functions                                #
+######################################################
+
+# BIC for this likelihood
+BIC_llo <- function(x, y, k, params = NA, lower = c(0.001, -5), upper = c(10,30)){
+  n <- length(x)
+  if(k == 0){
+    #suppressWarnings(if(is.na(params)) stop("must specify null params when k = 0"))
+    suppressWarnings(if(anyNA(params)) stop("must specify null params when k = 0"))
+
+    result <- -2*llo_lik(params = params, x = x, y = y, log = TRUE)
+  } else if(anyNA(params)){
+    # optBayes <- stats::optim(c(0.5, 0.5), llo_lik, x=x, y=y, log = TRUE, neg = TRUE, method = "L-BFGS-B",
+    #                   lower = lower, upper = upper)
+    # optBayes <- stats::optim(c(0.5, 0.5), llo_optim_wrap, x=x, y=y, method = "Nelder-Mead",
+    #                        neg = TRUE, log = TRUE)
+    optBayes <- llo_optim(x,y,lower,upper)
+    max_lik <- -optBayes$value
+    MLEs <- optBayes$par
+    result <- list(BIC = k * log(n) - (2 * max_lik),
+                   est_params = MLEs)
+  } else {
+    result <- list(BIC = k * log(n) - (2 * llo_lik(params = params, x = x, y = y, log = TRUE)),
+                   params = params)
+  }
+  return(result)
+}
+
+
+# Bayes factor - only approx BIC version for now
+bayes_factor <- function(BIC1, BIC2, approx = TRUE){
+  return(exp(-(1/2) * (BIC1 - BIC2)))
+}
+
+# Posterior model probability
+post_mod_prob <- function(BF){
+  return(1/(1+BF))
+}
+
 
 bayes_testing_dev <- function(x, y, k = 2, params_null = c(1,1), params = NA, optim_details = FALSE, lower = c(0.001, -5), upper = c(10,30)){
   # BIC under null
