8e3f10e99e48a35354c479afcee9c9cb167f1c68
apguthrie <54636090+apguthrie@users.noreply.github.com>
2024-02-20 16:01:17 -0500

    add foreclosure data INTERNALLY

---
+++ data-raw/foreclosure.R
@@ -0,0 +1,12 @@
+## code to prepare `foreclosure` dataset goes here
+
+foreclosure <- read.csv("../ppc/llo_method/llo_paper/case_studies/data/foreclosure_ex_oos.csv", row.names=1)
+colnames(foreclosure) <- c("y", "x", "year")
+
+# subset by just 5,000 obs from 2010
+foreclosure <- foreclosure[foreclosure$year==2010,]
+set.seed(47)
+inds <- sample(1:nrow(foreclosure), 5000)
+foreclosure <- foreclosure[inds,]
+
+usethis::use_data(foreclosure, internal=TRUE, overwrite = FALSE)
