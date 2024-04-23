a1ac9d8154c2dd6bb05cbc0cd78c8f9b06360425
apguthrie <54636090+apguthrie@users.noreply.github.com>
2024-04-23 10:46:36 -0400

    added examples (incomplete), modified how call to nloptr is returned for better transparency, updated ref to our paper, added details on optimization, documentation for epsilon

--- R/boldness_recalib.R
+++ R/boldness_recalib.R
@@ -2,37 +2,49 @@
 #  External Functions                                #
 ######################################################
 
-# also return br probs (add flag to toggle this?)
-# add option to see nlopt printing vs our own printing
+# fix up passing opts list to nloptr and finish examples
 
-# suppress nloprt output by default, add in details that if choose to print, will be in terms of tau (if that's what the bakeoff says)
-# otherwise report everything in terms of delta
 
-#' Boldness Recalibration for Binary Events
+#' Boldness-Recalibration for Binary Events
 #'
 #' Perform Bayesian boldness-recalibration as specified in Guthrie and Franck
-#' (2024).
+#' (2024). Boldness-recalibration maximizes the spread in predictions (`x`)
+#' subject to a constraint on the minimum tolerable posterior probability of
+#' calibration (`t`).
+#'
+#' The objective function in boldness-recalibration is \deqn{ f(\delta, \gamma)
+#' = -sd(\mathbf{x}')} and the constraint is \deqn{g(\delta, \gamma) =
+#' -(P(M_c|\mathbf{y}, \mathbf{x}')-t) \leq 0.}  As both the objective and
+#' constraint functions are non-linear with respect to \eqn{\delta} and
+#' \eqn{\gamma}, we use \link[nloptr]{nloptr} for this optimization rather than
+#' \link[stats]{optim}. Note that we use `x` to denote a vector of predicted
+#' probabilities, `nloptr()` uses `x` to denote the parameters being optimized.
+#' Thus, starting values for \eqn{\delta} and \eqn{\gamma} are passed via
+#' argument `x0` and all output refers to the objective and constraint as `f(x)`
+#' and `g(x)`.
 #'
 #' By default, this function uses the Augmented Lagrangian Algorithm (AUGLAG)
-#' (Conn et. al. 1991, Birgin and Martinez 2008) as the outer optimization routine
-#' and Sequential Least-Squares Quadratic Programming (SLSQP) (Dieter 1988, Dieter 1994)
-#' as the inner optimization routine.
+#' (Conn et. al. 1991, Birgin and Martinez 2008) as the outer optimization
+#' routine and Sequential Least-Squares Quadratic Programming (SLSQP) (Dieter
+#' 1988, Dieter 1994) as the inner optimization routine.
 #'
 #' For more control over the optimization routine conducted by `nloptr()`, the
-#' user may specify their own options via the `opts` argument.  Note that any objective,
-#' constraint, or gradient functions specified by the user will be overwritten
-#' by those specified in this package. See the documentation for `nloptr()` and
-#' the NLopt website for full details (<https://nlopt.readthedocs.io/en/latest/>).
+#' user may specify their own options via the `opts` argument.  Note that any
+#' objective, constraint, or gradient functions specified by the user will be
+#' overwritten by those specified in this package. See the documentation for
+#' `nloptr()` and the NLopt website for full details
+#' (<https://nlopt.readthedocs.io/en/latest/>).
 #'
 #' When `tau=TRUE`, the optimization routine operates relative to \eqn{\tau =
 #' log(\delta)} instead of \eqn{\delta}.  Specification of start location `x0`
 #' and bounds `lb`, `ub` should still be specified in terms of \eqn{\delta}. The
 #' `brcal` function will automatically convert from \eqn{\delta} to \eqn{\tau}.
-#' In the returned list, `BR_params` will always report in terms of \eqn{\delta}.
-#' However, `nloptr` will reflect whichever scale `nloptr()` optimized on.
+#' In the returned list, `BR_params` will always report in terms of
+#' \eqn{\delta}. However, the results returned in `nloptr` will reflect
+#' whichever scale `nloptr()` optimized on.
 #'
 #' @inheritParams bayes_ms
-#' @param t Desired level of calibration in \[0,1\].
+#' @param t Minimum tolerable level of calibration in \[0,1\].
 #' @param tau Logical.  If `TRUE`, the optimization operates on \eqn{\tau =
 #'   log(\delta)} instead of \eqn{\delta}. See details.
 #' @param start_at_MLEs Logical. If `TRUE`, the optimizer will start at
@@ -78,27 +90,76 @@
 #' @importFrom stats optim
 #' @importFrom nloptr nloptr
 #'
+#'
+#'
+#'
 #' @references Guthrie, A. P., and Franck, C. T. (2024) Boldness-Recalibration
-#'   for Binary Event Predictions. \emph{arxiv}.
+#'   for Binary Event Predictions, \emph{The American Statistician} 1-17.
 #'
-#'   Johnson, S. G., The NLopt nonlinear-optimization package, <https://nlopt.readthedocs.io/en/latest/>.
+#'   Johnson, S. G., The NLopt nonlinear-optimization package,
+#'   <https://nlopt.readthedocs.io/en/latest/>.
 #'
-#'   Conn, A. R., Gould, N. I. M., and Toint, P. L. (1991) A globally convergent augmented
-#'   Lagrangian algorithm for optimization with general constraints and simple bounds,
-#'   \emph{SIAM Journal of Numerical Analysis} vol. 28, no. 2, p. 545-572.
+#'   Conn, A. R., Gould, N. I. M., and Toint, P. L. (1991) A globally convergent
+#'   augmented Lagrangian algorithm for optimization with general constraints
+#'   and simple bounds, \emph{SIAM Journal of Numerical Analysis} vol. 28, no.
+#'   2, p. 545-572.
 #'
 #'   Birgin, E. G., and Martínez, J. M. (2008) Improving ultimate convergence of
 #'   an augmented Lagrangian method, \emph{Optimization Methods and Software}
 #'   vol. 23, no. 2, p. 177-195.
 #'
 #'   Kraft, D. (1988) A software package for sequential quadratic programming",
-#'   \emph{Technical Report} DFVLR-FB 88-28, Institut für Dynamik der Flugsysteme,
-#'   Oberpfaffenhofen.
+#'   \emph{Technical Report} DFVLR-FB 88-28, Institut für Dynamik der
+#'   Flugsysteme, Oberpfaffenhofen.
 #'
-#'   Kraft, D. (1994) Algorithm 733: TOMP–Fortran modules for optimal control
-#'   calculations, \emph{ACM Transactions on Mathematical Software}, vol. 20, no. 3, pp. 262-281 .
+#'   Kraft, D. (1994) Algorithm 733: TOMP-Fortran modules for optimal control
+#'   calculations, \emph{ACM Transactions on Mathematical Software}, vol. 20,
+#'   no. 3, pp. 262-281.
 #'
 #' @examples
+#' #' # Simulate 100 predicted probabilities
+#' x <- runif(100)
+#' # Simulated 100 binary event outcomes using x
+#' y <- rbinom(100, 1, x)  # By construction, x is well calibrated.
+#'
+#' # Perform 95% boldness-recalibration
+#' brcal(x, y)
+#'
+#' # Perform 90% boldness-recalibration
+#' brcal(x, y, t=0.9)
+#'
+#' # To specify different prior model probability of calibration, use Pmc
+#' # Prior model prob of 0.7:
+#' brcal(x, y, Pmc=0.7)
+#' # Prior model prob of 0.2
+#' brcal(x, y, Pmc=0.2)
+#'
+#' # To suppress all output from nloptr() for each iteration use print_level=0
+#' brcal(x, y, print_level=0)
+#'
+#' # For reduced output at each iteration used print_level=1 or 2
+#' brcal(x, y, print_level=1)
+#'
+#' # To specify different starting values, use x0 and set start_at_MLEs=FALSE
+#' #brcal(x, y, x0=c(1,2), start_at_MLEs=FALSE)
+#'
+#' # Adjust stopping criteria:
+#' # max number of evaluations (maxeval)
+#' brcal(x, y, opts=list(maxeval = 100))
+#'
+#' # What if events are defined by text instead of 0 or 1?
+#' y2 <- ifelse(y==0, "Loss", "Win")
+#' brcal(x, y2, event="Win", print_level=0)  # same result
+#'
+#' # What if we're interested in the probability of loss instead of win?
+#' x2 <- 1 - x
+#' brcal(x2, y2, event="Loss", print_level=0)
+#'
+#' # Push probabilities away from bounds by 0.000001
+#' x3 <- c(runif(50, 0, 0.0001), runif(50, .9999, 1))
+#' y3 <- rbinom(100, 1, 0.5)
+#' brcal(x3, y3, epsilon=0.000001)
+#' 
 brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
                   start_at_MLEs=TRUE, x0=NULL,
                   lb=c(0.00001, -Inf), ub=c(Inf, Inf),
@@ -106,6 +167,7 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
                   xtol_rel_inner=1.0e-6,
                   xtol_rel_outer=1.0e-6,
                   print_level=3,
+                  epsilon=.Machine$double.eps,
                   opts=NULL){
 
 
@@ -123,10 +185,10 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
   if(length(x) != length(y)) stop("x and y length differ")
 
   # check t is valid calibration prob
-  t <- check_input_probs(t, name="t")
+  t <- check_value01(t, name="t")
 
   # check Pmc is valid prior model prob
-  Pmc <- check_input_probs(Pmc, name="Pmc")
+  Pmc <- check_value01(Pmc, name="Pmc")
 
   # check tau is logical
   if(!is.logical(tau) & !(tau %in% c(0,1))){
@@ -149,13 +211,16 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
   # check upper and lower bounds
   lb <- check_input_params(lb, name="lb")
   ub <- check_input_params(ub, name="ub")
+  
+  # check epsilon
+  epsilon <- check_value01(epsilon, name="epsilon")
 
   ###################
   #  Function Code  #
   ###################
 
   if(start_at_MLEs){
-    bt <- bayes_ms_internal(x,y)
+    bt <- bayes_ms_internal(x,y, epsilon=epsilon)
     x0 <- bt$MLEs
   }
 
@@ -192,7 +257,8 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
                           outs = y,
                           t = t,
                           tau = tau,
-                          Pmc = Pmc)
+                          Pmc = Pmc, 
+                          epsilon=epsilon)
   } else {
 
     if("eval_f" %in% names(opts$local_opts)){
@@ -220,11 +286,28 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
                           outs = y,
                           t = t,
                           tau = tau,
-                          Pmc = Pmc)
+                          Pmc = Pmc, 
+                          epsilon=epsilon)
   }
 
   br_params <- c(res$solution[1], res$solution[2])
-
+  
+  # Make Call more useful when printed
+  res$call$x0 <- res$x0               
+  res$call$lb <- res$lb               
+  res$call$ub <- res$ub               
+  res$call$opts$maxeval <- maxeval     
+  res$call$opts$maxtime  <- res$options$maxtime    
+  res$call$opts$xtol_rel  <- res$options$xtol_rel    
+  res$call$opts$print_level  <- res$options$print_level
+  res$call$opts$local_opts$lb <- res$local_options$lb              
+  res$call$opts$local_opts$ub <- res$local_options$ub              
+  res$call$opts$local_opts$xtol_rel <- res$local_options$xtol_rel
+  res$call$Pmc <- Pmc 
+  res$call$t <- t
+  res$call$tau <- tau
+  res$call$epsilon <- epsilon
+  
   if(tau){
     br_params[1] <- exp(br_params[1])
   }
@@ -244,7 +327,7 @@ brcal <- function(x, y, t=0.95, Pmc=0.5, tau=FALSE, event=1,
 #  Internal Functions                                #
 ######################################################
 
-obj_f <- function(x, probs, outs, t, tau, Pmc){
+obj_f <- function(x, probs, outs, t, tau, Pmc, epsilon){
   if(tau)(
     x[1] <- exp(x[1])
   )
@@ -253,7 +336,7 @@ obj_f <- function(x, probs, outs, t, tau, Pmc){
   return(-stats::sd(probs_new))
 }
 
-obj_grad_f <- function(x, probs, outs, t, tau, Pmc){
+obj_grad_f <- function(x, probs, outs, t, tau, Pmc, epsilon){
   if(tau)(
     x[1] <- exp(x[1])
   )
@@ -279,23 +362,23 @@ obj_grad_f <- function(x, probs, outs, t, tau, Pmc){
   return(grad_obj)
 }
 
-constr_g <- function(x, probs, outs, t, tau, Pmc){
+constr_g <- function(x, probs, outs, t, tau, Pmc, epsilon){
   if(tau)(
     x[1] <- exp(x[1])
   )
   probs_new <- LLO_internal(x=probs, x[1], x[2])
-  c1 <- bayes_ms_internal(x=probs_new, y=outs, Pmc=Pmc)$posterior_model_prob * -1 + t
+  c1 <- bayes_ms_internal(x=probs_new, y=outs, Pmc=Pmc, epsilon=epsilon)$posterior_model_prob * -1 + t
   return(c1)
 }
 
-constr_grad_g <- function(x, probs, outs, t, tau, Pmc){
+constr_grad_g <- function(x, probs, outs, t, tau, Pmc, epsilon){
   if(tau)(
     x[1] <- exp(x[1])
   )
 
   n <- length(probs)
   probs_new <- LLO_internal(x=probs, x[1], x[2])
-  bt <- bayes_ms_internal(x=probs_new, y=outs, Pmc = Pmc)
+  bt <- bayes_ms_internal(x=probs_new, y=outs, Pmc = Pmc, epsilon=epsilon)
   pmp <- bt$posterior_model_prob
   pmp2 <- pmp^2
   dhat <- bt$MLEs[1]
