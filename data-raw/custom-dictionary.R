3e2554327c4ae598a0ae2ef0dbec3a3cd4d2c0f4
apguthrie <54636090+apguthrie@users.noreply.github.com>
2024-06-21 08:45:20 -0400

    add custom word list

---
+++ data-raw/custom-dictionary.R
@@ -0,0 +1,23 @@
+custom_user_spelling_dictionary = function(custom_words, pkg) {
+
+  # Adapted from tmsalab / edmdata
+  # Create an R-level dictionary using an RDS file.
+  saveRDS(custom_words, file = file.path(".aspell", paste0(pkg, ".rds")), version = 2)
+
+  # For the {spelling} package, write out a simple WORDLIST.
+  writeLines(custom_words, file.path("inst", "WORDLIST"))
+
+  # Provide backward compatibility support for base R
+  # Note, must be run from package root.
+  # For more details, see: ?"aspell-utils"
+  utils::aspell_write_personal_dictionary_file(
+    custom_words,
+    out = file.path(".aspell", "words.pws") 
+  )
+
+  invisible(custom_words)
+}
+
+custom_words = c("recalibration", "Recalibration", "MLE", "LLO", "Frequentist")
+
+custom_user_spelling_dictionary(custom_words, "BRcal")
